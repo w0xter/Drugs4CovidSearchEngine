1ddcb72f590fa33004f7b48a894cbceec35fc37f
w0xter <luispozogilo@gmail.com>
2020-04-15 00:53:31 +0200

    Fixing Deploy

---
+++ docs/precache-manifest.53244656565ab8789c8b4e1ec171cf74.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12fd49ccd8cae7dc134460798457f65c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33c42ecc78f17ac6a0d2",
+    "url": "/static/css/2.a43b59a1.chunk.css"
+  },
+  {
+    "revision": "4741156ab6a08a9d67ee",
+    "url": "/static/css/main.29e958ea.chunk.css"
+  },
+  {
+    "revision": "33c42ecc78f17ac6a0d2",
+    "url": "/static/js/2.2b735845.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2b735845.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4741156ab6a08a9d67ee",
+    "url": "/static/js/main.52b9dec8.chunk.js"
+  },
+  {
+    "revision": "28c836c6390ca2244059",
+    "url": "/static/js/runtime-main.3ce8a40d.js"
+  },
+  {
+    "revision": "c8aab4c4d06cae32a638bb6121d64710",
+    "url": "/static/media/medical_research.c8aab4c4.svg"
+  }
+]);
