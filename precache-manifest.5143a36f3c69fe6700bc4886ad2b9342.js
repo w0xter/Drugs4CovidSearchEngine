952b78069688fca6077b22a35cb06e65e7e0d11f
w0xter <luispozogilo@gmail.com>
2020-04-14 22:57:21 +0200

    Preparing Deploy

---
+++ precache-manifest.5143a36f3c69fe6700bc4886ad2b9342.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d62e0234ef75fe552d2268b04f13f85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e27709b461a6a2dfb035",
+    "url": "/static/css/2.a43b59a1.chunk.css"
+  },
+  {
+    "revision": "8141411102c5fb722f71",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e27709b461a6a2dfb035",
+    "url": "/static/js/2.391198d5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.391198d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8141411102c5fb722f71",
+    "url": "/static/js/main.d63ce266.chunk.js"
+  },
+  {
+    "revision": "28c836c6390ca2244059",
+    "url": "/static/js/runtime-main.3ce8a40d.js"
+  }
+]);
