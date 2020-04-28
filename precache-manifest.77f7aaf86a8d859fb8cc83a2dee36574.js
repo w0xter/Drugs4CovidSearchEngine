9421c019f0441babdd1bcae7f4b08033af9f2796
w0xter <luispozogilo@gmail.com>
2020-04-28 17:46:20 +0200

    Updates

---
+++ precache-manifest.77f7aaf86a8d859fb8cc83a2dee36574.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45122a2281deda019e55098a0bf04c0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46744a056398e42b41c4",
+    "url": "/static/css/2.a43b59a1.chunk.css"
+  },
+  {
+    "revision": "4644f8169e81ed900c84",
+    "url": "/static/css/main.d4220677.chunk.css"
+  },
+  {
+    "revision": "46744a056398e42b41c4",
+    "url": "/static/js/2.b59af5bc.chunk.js"
+  },
+  {
+    "revision": "5ad387cd57eb31f6f50768be47fe3b09",
+    "url": "/static/js/2.b59af5bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4644f8169e81ed900c84",
+    "url": "/static/js/main.983a783d.chunk.js"
+  },
+  {
+    "revision": "28c836c6390ca2244059",
+    "url": "/static/js/runtime-main.3ce8a40d.js"
+  },
+  {
+    "revision": "403de342af783727331beb7b1695160e",
+    "url": "/static/media/abduction.403de342.svg"
+  },
+  {
+    "revision": "aa6b7ebb57205ee68e256f6b75927bf9",
+    "url": "/static/media/logo.aa6b7ebb.svg"
+  },
+  {
+    "revision": "c8aab4c4d06cae32a638bb6121d64710",
+    "url": "/static/media/medical_research.c8aab4c4.svg"
+  }
+]);
